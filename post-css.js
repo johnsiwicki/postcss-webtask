ccab5b891fa37d7426fe116db3af629596abdd12
John Siwicki <johnsiwicki@gmail.com>
2018-04-17 22:07:49 -0400

    Updating post-css.js

--- post-css.js
+++ post-css.js
@@ -1,11 +1,14 @@
 const postcss = require('postcss');
 const cssvariables = require('postcss-css-variables');
+const pFor = require('postcss-for');
+
 
 module.exports = function(context, cb) {
   const mycss = context.body.email;
   
   var output = postcss([
-		cssvariables(/*options*/)
+		cssvariables(/*options*/),
+		pFor()
 	])
 	.process(mycss)
 	.css;
